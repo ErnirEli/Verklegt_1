ef53f85356d6cbbc1eae57cd6595702e24b3e46d
AlexanderBjorns <alexanderb25@ru.is>
2025-12-05 16:14:58 +0000

    validation changes

--- validation/player_validation.py
+++ validation/player_validation.py
@@ -6,54 +6,65 @@
 # link
 # handle
 # team
-
+from models.player import Player
 from Datalayer.data_api import DataAPI
 
 
 class ValidatePlayer:
 
-    
 
-    def __init__(self):
-        pass
+
+    def __init__(self, data_wrapper: DataAPI):
+        self._data = data_wrapper
+    
 
     def is_captain_or_organizer():
         #Á eftir að implementa
         return
     
     def validate_name(self, name:str):
-        if not name:
+        if not name or name.strip() == "":
             return (False, "PLayer needs to have a name")
+        return True
     
     def validate_age(self, age: str):
-        age = int(age)
-        if age < #-----------------------------------------> Ætlum við að hafa age limit??  bæði of amall of ungur?
-            return (False, "")
-        return True,
+        try:
+            age = int(age)
+        except ValueError():
+            return "Please input a number"
+        if age < 18 or age > 65: #-----------------------------------------> Ætlum við að hafa age limit??  bæði of amall of ungur?
+            return (False, "Player is too young or old to participate")
+        return True
 
     def validate_home_adress(self, adress: str):
-        if not adress:
+        if not adress or adress.strip() == "":
             return (False, "Player needs to have a home adress")
-        #Eitthvað miera?
-
         return True,
 
     def validate_email(self, email: str):
-        if not email:
+        if not email or email.strip() == "":
             return (False, "Player needs to have an email")
         if "@" and "." not in email and not isinstance(email, str):
             return (False, "Email is invalid")
         return True,
 
     def validate_link(self, link: str):
-        if not link:
+        if not link or link.strip() == "":
             return (False, "Player needs to have a link")
+        if link.startswith("https://") == False:
+            return (False, "Link is not valid try again")
+        return True
         #eitthvað fleira??
 
-    def validate_handle(self, handle: str):
-         handle_list = DataAPI.get_all_handles()
-         if handle in handle_list:
-             return (False, "Player handle needs to be unique")
-         if not handle:
-             return (False, "Player needs to have a handle")
-         return True,
+    def validate_handle(self, players, handle: str):
+        players = self._data.get_all_players()
+        for line in players:
+            if line.handle == handle:
+                return (False, "Player handle needs to be unique")
+            if handle == None:
+                return (False, "Player needs to have a handle")
+            for ch in handle:
+                if "," in handle or "/" in handle:
+                    return(False, "Username cannot contain , or / in name try again")
+        return True
+            
