9fe15b041c0eaaf7c397e294ac39bb291dbddf8e
AlexanderBjorns <alexanderb25@ru.is>
2025-12-05 16:32:53 +0000

    changes

--- validation/player_validation.py
+++ validation/player_validation.py
@@ -31,7 +31,7 @@ class ValidatePlayer:
         try:
             age = int(age)
         except ValueError():
-            return "Please input a number"
+            return (False, "Age must be a number")
         if age < 18 or age > 65: #-----------------------------------------> Ætlum við að hafa age limit??  bæði of amall of ungur?
             return (False, "Player is too young or old to participate")
         return True
@@ -44,8 +44,9 @@ class ValidatePlayer:
     def validate_email(self, email: str):
         if not email or email.strip() == "":
             return (False, "Player needs to have an email")
-        if "@" and "." not in email and not isinstance(email, str):
-            return (False, "Email is invalid")
+        if "@" not in email or "." not in email:
+            return False, "Email is invalid"
+
         return True,
 
     def validate_link(self, link: str):
@@ -56,15 +57,14 @@ class ValidatePlayer:
         return True
         #eitthvað fleira??
 
-    def validate_handle(self, players, handle: str):
+    def validate_handle(self, handle: str):
+        if not handle or handle.strip() == "":
+            return (False, "Player needs to have a handle")
         players = self._data.get_all_players()
+        if "," in handle or "/" in handle:
+            return (False, "Handle contains illegal characters like ',' and '/'")
         for line in players:
             if line.handle == handle:
                 return (False, "Player handle needs to be unique")
-            if handle == None:
-                return (False, "Player needs to have a handle")
-            for ch in handle:
-                if "," in handle or "/" in handle:
-                    return(False, "Username cannot contain , or / in name try again")
-        return True
+        return True, ""
             
