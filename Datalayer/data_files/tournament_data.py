2a5c18c4c6f1e223ea24c57d5bd3dbb6f5474ac4
olgeirotri <olgeirotri@gmail.com>
2025-12-08 11:49:56 +0000

    ui dot, organizer og eith

--- Datalayer/data_files/tournament_data.py
+++ Datalayer/data_files/tournament_data.py
@@ -1,5 +1,5 @@
 import csv
-from models import Tournament
+from models.tournament import Tournament
 
 
 class TournamentFiles():
--- validation/player_validation.py
+++ validation/player_validation.py
@@ -6,7 +6,7 @@
 # link
 # handle
 # team
-from models.player import Player
+
 from Datalayer.data_api import DataAPI
 
 
@@ -14,8 +14,8 @@ class ValidatePlayer:
 
 
 
-    def __init__(self, data_wrapper: DataAPI):
-        self._data = data_wrapper
+    def __init__(self):
+        self._data = DataAPI()
     
 
     def is_captain_or_organizer():
@@ -24,14 +24,16 @@ class ValidatePlayer:
     
     def validate_name(self, name:str):
         if not name or name.strip() == "":
-            return (False, "PLayer needs to have a name")
+            return (False, "Player needs to have a name")
         return True,
     
     def validate_age(self, age: str):
+        #if not age.isnumeric():
+        
         try:
-            age = int(age)
-        except ValueError():
-            return (False, "Age must be a number")
+             age = int(age)
+        except ValueError:
+            return (False, "Age must be a number, try again")
         if age < 18 or age > 65: #-----------------------------------------> Ætlum við að hafa age limit??  bæði of amall of ungur?
             return (False, "Player is too young or old to participate")
         return True,
@@ -41,11 +43,18 @@ class ValidatePlayer:
             return (False, "Player needs to have a home adress")
         return True,
 
+    def validate_phone(self, phone: str):
+        if phone == "354":
+            return (False, "Player needs to have a phone number")
+        if not phone.isnumeric() or len(phone) != 10:
+            return (False, "Phone number is ivalid, try again")
+        return True,
+
     def validate_email(self, email: str):
         if not email or email.strip() == "":
             return (False, "Player needs to have an email")
         if "@" not in email or "." not in email:
-            return False, "Email is invalid"
+            return (False, "Email is invalid, try again")
 
         return True,
 
@@ -53,7 +62,9 @@ class ValidatePlayer:
         if not link or link.strip() == "":
             return (False, "Player needs to have a link")
         if link.startswith("https://") == False:
-            return (False, "Link is not valid try again")
+            return (False, "Link is not valid, try again")
+        if "." not in link:
+            return (False, "Link is not valid, try again")
         return True,
         #eitthvað fleira??
 
@@ -62,9 +73,8 @@ class ValidatePlayer:
             return (False, "Player needs to have a handle")
         players = self._data.get_all_players()
         if "," in handle or "/" in handle:
-            return (False, "Handle contains illegal characters like ',' and '/'")
+            return (False, "Handle contains illegal characters like ',' and '/', try again")
         for line in players:
             if line.handle == handle:
-                return (False, "Player handle needs to be unique")
+                return (False, "Player handle needs to be unique, try again")
         return True,
-            
