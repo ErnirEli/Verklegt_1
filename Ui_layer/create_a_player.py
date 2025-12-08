3815891f21532dbb1d445b17daa9097fb47eb6b0
olgeirotri <olgeirotri@gmail.com>
2025-12-08 09:51:59 +0000

    kldskjf

---
+++ Ui_layer/create_a_player.py
@@ -0,0 +1,17 @@
+from validation.player import Player
+
+
+class CreatePlayer:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return (
+            "Name:" \
+            "Date of birth" \
+            "Home address" \
+            "Phone Number:" \
+            "Hadle:" \
+            "Email:" \
+            ""
+        )
