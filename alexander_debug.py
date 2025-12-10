759245c638cf447b237e3624acf858309a874984
AlexanderBjorns <alexanderb25@ru.is>
2025-12-10 10:51:05 +0000

    Update alexander_debug.py

--- alexander_debug.py
+++ alexander_debug.py
@@ -1,11 +1,27 @@
-# from Datalayer.data_api import DataAPI
-# from logic.team_logic import TeamLogic
-# from models.team import Team
-# from models.player import Player
-from Ui_layer.organizer_ui import *
+from logic.logic_api import logicAPI
 
-yo = OrganizerUI()
 
-choice = yo.get_choice()
-if choice == "7":
-    yo.edit_player_info()
+class SpectatorUI:
+
+    def __init__(self, logic: logicAPI):
+        self._logic = logic
+
+    def run(self):
+        while True:
+            print("\n=== Spectator Menu ===")
+            print("1. List players")
+            print("2. View player info")
+            print("3. List tournaments")
+            print("0. Back / exit")
+            choice = input("Choose an option: ").strip()
+
+            if choice == "1":
+                self.list_players()
+            elif choice == "2":
+                self.view_player()
+            elif choice == "3":
+                self.list_tournaments()
+            elif choice == "0":
+                break
+            else:
+                print("Invalid choice, try again.")
