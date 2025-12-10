f122a12e19d51883f076306062bf787bdf62c591
AlexanderBjorns <alexanderb25@ru.is>
2025-12-10 13:10:18 +0000

    changes

--- alexander_debug2.py
+++ alexander_debug2.py
@@ -1,12 +1,11 @@
-from logic.logic_api import logicAPI
+
 from alexander_debug import SpectatorUI
 
 
 def main():
-    logic = logicAPI()
+
     spec_ui = SpectatorUI()
     spec_ui.run()
-
 
 if __name__ == "__main__":
     main()
